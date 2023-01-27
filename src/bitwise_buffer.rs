241624ea4373bf038e8db32f944c217d8096dc45
zaitam <gotzmatiascode@gmail.com>
2023-01-26 18:08:01 -0800

    BitwiseBuffer update

--- src/bitwise_buffer.rs
+++ src/bitwise_buffer.rs
@@ -1,44 +1,63 @@
-pub struct BitwiseBuffer {
+struct BitwiseBuffer {
     buffer: Vec<u8>,
-    bit_count: usize,
+    remaining_bits: usize,
     current_byte: u8,
 }
 
-pub struct EncodedBit<T> {
-    pub(crate) data: T,
-    pub(crate) bit_len: usize,
+struct EncodedBit {
+    data: u8, //TODO: Make this work for u8, u16, u32, u64
+    bit_len: usize,
 }
+/*enum Unsigned {
+    U16(u16),
+    U32(u32),
+    U64(u64),
+}*/
 
 impl BitwiseBuffer {
-    pub fn new() -> BitwiseBuffer {
+    fn new() -> BitwiseBuffer {
         BitwiseBuffer {
             buffer: vec![],
-            bit_count: 0,
+            remaining_bits: 4,
             current_byte: 0,
         }
     }
 
-    pub fn push_bits<T>(&mut self, bits: Vec<EncodedBit<T>>) where T: Into<u8> {
+    fn push_bits<T: Into<Unsigned>>(&mut self, bits: Vec<EncodedBit>) {
         for encoded_bit in bits {
-            self.current_byte |= encoded_bit.data << (8 - self.bit_count - encoded_bit.bit_len);
-            self.bit_count += encoded_bit.bit_len;
-            if self.bit_count >= 8 {
-                self.push_last_bit();
+            if encoded_bit.bit_len <= self.remaining_bits { //If the data enters in remaining bits
+                self.push_to_curr_bit(encoded_bit.data, encoded_bit.bit_len);
+                if self.remaining_bits == 0 {
+                    self.push_bit();
+                }
+            } else {
+                // usize division does a floor func automatically so no need to floor or ciel, just add 7
+                let iterations = (encoded_bit.bit_len - self.remaining_bits + 7)/8;
+                // first iter
+                let n = encoded_bit.bit_len - self.remaining_bits;
+                self.push_to_curr_bit(encoded_bit.data >> (8 - self.remaining_bits), self.remaining_bits);
+                self.push_bit();
+                // Doesn't include first and last iterations
+                for i in 1..iterations {
+                    self.push_to_curr_bit( (encoded_bit.data >> n - (i * 8)) as u8, 8);
+                    self.push_bit();
+                }
+                // last iter
+                let len = n - ((iterations - 1) * 8);
+                self.push_to_curr_bit((((encoded_bit.data << (8 - len)) as u8) >> (8 - len)) ,len);
             }
         }
     }
 
-    fn push_last_bit(&mut self) {
-        self.buffer.push(self.current_byte);
-        self.current_byte = 0;
-        self.bit_count -= 8;
+    fn push_to_curr_bit(&mut self, data: u8, len: usize) {
+        self.current_byte = (self.current_byte << len) + data;
+        self.remaining_bits -= len;
     }
 
-    fn flush(&mut self) -> Vec<u8> {
-        if self.bit_count > 0 {
-            self.buffer.push(self.current_byte << (8 - self.bit_count));
-        }
-        std::mem::replace(&mut self.buffer, vec![])
+    fn push_bit(&mut self) {
+        self.buffer.push(self.current_byte);
+        self.current_byte = 0;
+        self.remaining_bits = 8;
     }
 }
 
